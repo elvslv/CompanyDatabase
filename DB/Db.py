490a49c840a1db7755003f4b9a292303f6b98373
Elena Vasilyeva <elvslv@gmail.com>
2011-11-11 15:20:31 +1100

    add table indexes

--- DB/Db.py
+++ DB/Db.py
@@ -16,6 +16,8 @@ string = lambda len: Column(String(len))
 uniqString = lambda len: Column(String(len), unique=True, nullable=False)
 pkey = lambda: Column(Integer, primary_key=True)
 fkey = lambda name: Column(Integer, ForeignKey(name, onupdate='CASCADE', ondelete='CASCADE'))
+pkeyIndex = lambda: Column(Integer, primary_key=True, index = True)
+fkeyIndex = lambda name: Column(Integer, ForeignKey(name, onupdate='CASCADE', ondelete='CASCADE'), index = True)
 
 DATABASE_HOST = "localhost"
 DATABASE_USER = "admin"
@@ -27,6 +29,159 @@ DB_STRING = """mysql+mysqldb://%s:%s@%s:%d/%s""" % \
 (DATABASE_USER, DATABASE_PASSWD, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME)
 
 
+class Company(Base):
+	__tablename__ = 'companies'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	id = pkeyIndex()
+	name = Column(Text, nullable = False)
+	details = Column(Text, nullable = False)
+
+	def __init__(self, name, details):
+		self.name = name
+		self.details = details
+
+class User(Base):
+	__tablename__ = 'users'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	login = Column(String(MAX_LOGIN_LENGTH), primary_key = True)
+	password = Column(Text, nullable = False)
+	admin = Column(Boolean, default = False)
+
+	def __init__(self, login, password, admin = False):
+		self.login = login
+		self.password = password
+		self.admin = admin
+
+class Employee(Base):
+	__tablename__ = 'employees'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	id = pkeyIndex()
+	name = Column(Text, nullable = False)
+	companyId = fkeyIndex('companies.id')
+	login = Column(String(MAX_LOGIN_LENGTH), 
+		ForeignKey('users.login', onupdate='CASCADE', ondelete='CASCADE'))
+
+	company = relationship(Company, backref=backref('employees', cascade = "all,delete"))
+	user = relationship(User, backref=backref('employee', useList = False, 
+		cascade = "all,delete"))
+	
+	def __init__(self, name, companyId, login):
+		self.name = name
+		self.companyId = companyId
+		self.login = login
+
+class Project(Base):
+	__tablename__ = 'projects'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	id = pkeyIndex()
+	name = Column(Text, nullable = False)
+	startDate = Column(DateTime, nullable = False)
+	stage = Column(Integer, nullable = False, default = STAGE_PROJECT_NOT_STARTED)
+	
+	def __init__(self, name, startDate, stage = STAGE_PROJECT_NOT_STARTED):
+		self.name = name
+		self.startDate = startDate
+		self.stage = stage
+
+class Contract(Base):
+	__tablename__ = 'contracts'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	id = pkeyIndex()
+	companyId = fkeyIndex('companies.id')
+	projectId = fkeyIndex('projects.id')
+	activity = Column(Integer, default = ACTIVITY_CONTRACT_NOT_MADE)
+
+	company = relationship(Company, backref=backref('contracts', cascade = "all,delete"))
+	project = relationship(Project, backref=backref('contracts', cascade = "all,delete"))
+	
+	def __init__(self, companyId, projectId, stage = ACTIVITY_CONTRACT_NOT_MADE):
+		self.companyId = companyId
+		self.projectId = projectId
+		self.activity = activity
+
+class ProjectEmployee(Base):
+	__tablename__ = 'projectEmployees'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+
+	employeeId = Column(Integer, ForeignKey('employees.id'), primary_key=True,
+		index = True)
+	projectId = Column(Integer, ForeignKey('projects.id'), primary_key=True,
+		index = True)
+	role = Column(Integer, default = ROLE_DEVELOPER)
+
+	employee = relationship(Employee, backref=backref('projects', cascade = "all,delete"))
+	project = relationship(Project, backref=backref('employees', cascade = "all,delete"))
+	
+	def __init__(self, employeeId, projectId, role = ROLE_DEVELOPER):
+		self.employeeId = employeeId
+		self.projectId = projectId
+		self.role = role
+
+class Task(Base):
+	__tablename__ = 'tasks'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	id = pkeyIndex()
+	name = Column(Text, nullable = False)
+	projectId = fkeyIndex('projects.id')
+	plannedTime = Column(Integer, nullable = False)
+	completionDate = Column(DateTime)
+
+	project = relationship(Project, backref=backref('tasks', cascade = "all,delete"))
+	
+	def __init__(self, name, projectId, plannedTime):
+		self.name = name
+		self.projectId = projectId
+		self.plannedTime = plannedTime
+
+class Job(Base):
+	__tablename__ = 'jobs'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+
+	employeeId = Column(Integer, ForeignKey('employees.id'), primary_key=True, 
+		index = True)
+	taskId = Column(Integer, ForeignKey('tasks.id'), primary_key=True, 
+		index = True)
+	startDate = Column(DateTime)
+	completionDate = Column(DateTime)
+	description = Column(Text)
+
+	employee = relationship(Employee, backref=backref('jobs', cascade = "all,delete"))
+	task = relationship(Task, backref=backref('jobs', cascade = "all,delete"))
+
+	def __init__(self, employeeId, projectId, startDate, completionDate, description):
+		self.employeeId = employeeId
+		self.projectId = projectId
+		self.startDate = startDate
+		self.completionDate = completionDate
+		self.description = description
+
+
+class TasksDependency(Base):
+	__tablename__ = 'tasksDependencies'
+	__table_args__ = {'mysql_engine':'InnoDB'}
+	
+	masterId = Column(Integer, ForeignKey('tasks.id'), primary_key=True, 
+		index = True)
+	slaveId = Column(Integer, ForeignKey('tasks.id'), primary_key=True,
+		index = True)
+
+	master = relationship(Task, foreign_keys=[masterId], 
+		backref=backref('slaves', cascade = "all,delete"))
+	slave = relationship(Project, foreign_keys=[slaveId], 
+		backref=backref('masters', cascade = "all,delete"))
+
+	def __init__(self, masterId, slaveId):
+		self.masterId = masterId
+		self.slaveId = slaveId
+
+
 class Database:
 	instance = None
 	engine = create_engine(DB_STRING, convert_unicode=True, echo = True,
@@ -34,7 +189,6 @@ class Database:
 
 	def __init__(self):
 		Base.metadata.create_all(self.engine)
-		self.Session = scoped_session(sessionmaker(bind=self.engine))
 
 	def commit(self):
 		self.session.commit()
@@ -84,149 +238,3 @@ def db_instance():
 	return Database.instance
 
 dbi = db_instance()
-
-class Company(Base):
-	__tablename__ = 'Companies'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	id = pkey()
-	name = Column(Text, nullable = False)
-	details = Column(Text, nullable = False)
-
-	def __init__(self, name, details):
-		self.name = name
-		self.details = details
-
-class User(Base):
-	__tablename__ = 'Users'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	login = Column(String(MAX_LOGIN_LENGTH), primary_key = True)
-	password = Column(Text, nullable = False)
-	admin = Column(Boolean, default = False)
-
-	def __init__(self, login, password, admin = False):
-		self.login = login
-		self.password = password
-		self.admin = admin
-
-class Employee(Base):
-	__tablename__ = 'Employees'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	id = pkey()
-	name = Column(Text, nullable = False)
-	companyId = fkey('companies.id')
-	login = Column(String(MAX_LOGIN_LENGTH), 
-		ForeignKey('users.login', onupdate='CASCADE', ondelete='CASCADE'))
-
-	company = relationship(Company, backref=backref('employees', cascade = "all,delete"))
-	user = relationship(User, backref=backref('employee', useList = False, 
-		cascade = "all,delete"))
-	
-	def __init__(self, name, companyId, login):
-		self.name = name
-		self.companyId = companyId
-		self.login = login
-
-class Project(Base):
-	__tablename__ = 'Projects'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	id = pkey()
-	name = Column(Text, nullable = False)
-	startDate = Column(DateTime, nullable = False)
-	stage = Column(Integer, nullable = False, default = STAGE_PROJECT_NOT_STARTED)
-	
-	def __init__(self, name, startDate, stage = STAGE_PROJECT_NOT_STARTED):
-		self.name = name
-		self.startDate = startDate
-		self.stage = stage
-
-class Contract(Base):
-	__tablename__ = 'Contracts'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	id = pkey()
-	companyId = fkey('companies.id')
-	projectId = fkey('projects.id')
-	activity = Column(Integer, default = ACTIVITY_CONTRACT_NOT_MADE)
-
-	company = relationship(Company, backref=backref('contracts', cascade = "all,delete"))
-	project = relationship(Project, backref=backref('contracts', cascade = "all,delete"))
-	
-	def __init__(self, companyId, projectId, stage = ACTIVITY_CONTRACT_NOT_MADE):
-		self.companyId = companyId
-		self.projectId = projectId
-		self.activity = activity
-
-class ProjectEmployee(Base):
-	__tablename__ = 'ProjectEmployees'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-
-	employeeId = Column(Integer, ForeignKey('employees.id'), primary_key=True)
-	projectId = Column(Integer, ForeignKey('projects.id'), primary_key=True)
-	role = Column(Integer, default = ROLE_DEVELOPER)
-
-	employee = relationship(Employee, backref=backref('projects', cascade = "all,delete"))
-	project = relationship(Project, backref=backref('employees', cascade = "all,delete"))
-	
-	def __init__(self, employeeId, projectId, role = ROLE_DEVELOPER):
-		self.employeeId = employeeId
-		self.projectId = projectId
-		self.role = role
-
-class Task(Base):
-	__tablename__ = 'Tasks'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	id = pkey()
-	name = Column(Text, nullable = False)
-	projectId = fkey('projects.id')
-	plannedTime = Column(Integer, nullable = False)
-	completionDate = Column(DateTime)
-
-	project = relationship(Project, backref=backref('tasks', cascade = "all,delete"))
-	
-	def __init__(self, name, projectId, plannedTime):
-		self.name = name
-		self.projectId = projectId
-		self.plannedTime = plannedTime
-
-class Job(Base):
-	__tablename__ = 'Jobs'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-
-	employeeId = Column(Integer, ForeignKey('employees.id'), primary_key=True)
-	taskId = Column(Integer, ForeignKey('tasks.id'), primary_key=True)
-	startDate = Column(DateTime)
-	completionDate = Column(DateTime)
-	description = Column(Text)
-
-	employee = relationship(Employee, backref=backref('jobs', cascade = "all,delete"))
-	task = relationship(Task, backref=backref('jobs', cascade = "all,delete"))
-
-	def __init__(self, employeeId, projectId, startDate, completionDate, description):
-		self.employeeId = employeeId
-		self.projectId = projectId
-		self.startDate = startDate
-		self.completionDate = completionDate
-		self.description = description
-
-
-class TasksDependency(Base):
-	__tablename__ = 'TasksDependencies'
-	__table_args__ = {'mysql_engine':'InnoDB'}
-	
-	masterId = Column(Integer, ForeignKey('tasks.id'), primary_key=True)
-	slaveId = Column(Integer, ForeignKey('tasks.id'), primary_key=True)
-
-	master = relationship(Task, foreign_keys=[masterId], 
-		backref=backref('slaves', cascade = "all,delete"))
-	slave = relationship(Project, foreign_keys=[slaveId], 
-		backref=backref('masters', cascade = "all,delete"))
-
-	def __init__(self, masterId, slaveId):
-		self.masterId = masterId
-		self.slaveId = slaveId
